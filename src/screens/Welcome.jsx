07a4f24074fc311357163ae89cb4a61ab4284317
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-23 02:17:38 +0100

    welcome screen is done :fire:

--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,18 +1,25 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button, Alert  } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'twrnc'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native';
 
 export const Welcome = () => {
     return (
-        <SafeAreaView>
-            <View style={tw`w-4/5 h-36 flex flex-row justify-center mt-5`}>
-                <TouchableOpacity>
-                <View style={tw`border-4 border-red-600 h-26 w-26 rounded-full flex flex-row justify-center items-center`}>
-                    <Text style={tw`text-2xl`}>A+</Text>
-                </View>
-
+        <SafeAreaView style={tw`flex items-center h-full w-full`}>
+            <View style={tw`w-[90%] h-[70%] flex justify-center items-center mt-2`}>
+                <Image source={require("../../assets/logo/icon-logo.png")} style={tw`h-36 w-36 mb-2`}></Image>
+                <Text style={tw`text-lg text-center uppercase font-900 tracking-widest`}>welcome to demi app</Text>
+            </View>
+            <View>
+                
+            </View>
+            <View style={tw`h-[20%] w-[90%] flex justify-evenly items-center`}>
+                <TouchableOpacity style={tw`w-[80%] bg-red-400 p-4 flex items-center rounded-md`}>
+                    <Text style={tw`text-md text-white w-full text-center`}>Sign In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={tw`w-[80%] border-2 border-red-400 p-4 flex items-center rounded-md`}>
+                    <Text style={tw`text-md text-red-400 w-full text-center`}>Create Account</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
