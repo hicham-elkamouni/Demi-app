6b69cf84ef60dcdc537a929699441defa52cea95
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-26 14:19:17 +0100

    bug: store export

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Navigation from './src/navigation';
-import { Provider } from 'react-redux';
-import { store } from './src/app/store';
-import { useCachedResources } from './src/hooks/useCachedResources';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Navigation from "./src/navigation";
+import { Provider } from "react-redux";
+import store from "./src/app/store";
+import { useCachedResources } from "./src/hooks/useCachedResources";
 
-import { LogBox } from 'react-native';
+import { LogBox } from "react-native";
 
-LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
-LogBox.ignoreLogs(['Setting a timer']);
+LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core"]);
+LogBox.ignoreLogs(["Setting a timer"]);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
