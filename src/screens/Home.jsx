5f31b7d9627b38446e26b3841d9e07dd84f7c6d3
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-27 02:46:02 +0100

    adding posts is done :fire: :fire :fire:

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,133 +1,81 @@
-import React, { useEffect, useState, useRef } from "react";
-import { StatusBar } from "expo-status-bar";
-import LottieView from "lottie-react-native";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  Image,
-  RefreshControl,
-  TouchableOpacity,
-  ScrollView,
-  View,
-} from "react-native";
-import { COLORS, SIZES } from "../constants/theme";
-//navigation
-import { useNavigation } from "@react-navigation/native";
-import tw from "twrnc";
-import { Ionicons } from '@expo/vector-icons';
-import { Button, Icon } from "react-native-elements";
-import { useSelector } from "react-redux";
-import { FloatingAction } from "react-native-floating-action";
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'twrnc'
+import { Divider } from 'react-native-paper'
 
-
-const wait = (timeout) => {
-  return new Promise((resolve) => setTimeout(resolve, timeout));
-};
-
-export default function Home() {
-  const [refreshing, setRefreshing] = useState(false);
-  const animation = useRef(null);
-  useEffect(() => {
-    // You can control the ref programmatically, rather than using autoPlay
-    // animation.current?.play();
-  }, []);
-
-
-  const actions = [
-    {
-      text: "Create Post",
-      icon: require("./../../assets/png/heart.png"),
-      name: "NewPost",
-      position: 1
-    },
-  ]
-
-
-
-  const cart = useSelector((state) => state.cart);
-
-  const [Food, setFood] = useState([]);
-  const [menu, setMenu] = useState([]);
-  const navigation = useNavigation();
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
-
-  const getAllFood = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var graphql = JSON.stringify({
-      query:
-        "query Query {\r\n  getAllProducts {\r\n    id\r\n    name\r\n    description\r\n    image\r\n    price\r\n    status\r\n    createdAt\r\n  }\r\n}",
-      variables: {},
-    });
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: graphql,
-      redirect: "follow",
-    };
-
-    fetch("https://foodaly.herokuapp.com/gql", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        setFood(result.data.getAllProducts);
-      })
-      .catch((error) => console.log("error", error));
-  };
-
-  // const getAllMenu = () => {
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("Content-Type", "application/json");
-
-  //   var graphql = JSON.stringify({
-  //     query:
-  //       "query GetAllMenu {\r\n  getAllMenu {\r\n    id\r\n    name\r\n    productIds {\r\n      id\r\n      name\r\n      image\r\n    price\r\n    image\r\n    description}\r\n  }\r\n}",
-  //     variables: {},
-  //   });
-  //   var requestOptions = {
-  //     method: "POST",
-  //     headers: myHeaders,
-  //     body: graphql,
-  //     redirect: "follow",
-  //   };
-
-  //   fetch("https://foodaly.herokuapp.com/gql", requestOptions)
-  //     .then((response) => response.json())
-  //     .then((result) => setMenu(result.data.getAllMenu))
-  //     .catch((error) => console.log("error", error));
-  // };
-
-  // useEffect(() => {
-  //   getAllFood();
-  //   getAllMenu();
-  // }, [refreshing]);
-
+const Home = () => {
   return (
-    <View
-    style={{
-      flex: 1,
-      position: "relative",
-    }}
-    >
-     <FloatingAction
-        actions={actions}
-        color={COLORS.primary}
-        onPressItem={name => {
-          navigation.navigate("AddPost");
-        }}
-      />
-    </View>
+    <View style={tw`mt-4 mb-6 w-full`}>
 
-  );
+      <ScrollView>
+
+        <View style={tw`flex items-center mb-4`}>
+
+          <View style={tw`w-[90%] border-2 border-red-200 rounded-md p-2 bg-white`}>
+            <View style={tw``}>
+              
+              {/* POST HEADER */}
+              <View style={tw`flex flex-row items-center mb-4`}>
+                <Image source={require("../../assets/blank-profile-picture.png")} style={tw`h-12 w-12 rounded-full border-2 border-red-200 mr-4`}></Image>
+                <View>
+                  <Text style={tw`text-lg text-gray-600 font-bold tracking-wider`}>John Doe</Text>
+                  <Text style={[tw`text-[4] text-gray-600 `, {fontSize : 14}]}>26-07-2022</Text>
+                </View>
+              </View>
+
+              {/* POST CONTENT */}
+              <View style={tw``}>
+                <Text style={[tw`mb-2`, {fontSize:20 }]}>I'm Looking for someone who had this blood type</Text>
+                  
+                  {/* BLOOD TYPES */}
+                <View style={tw`flex flex-row justif-around`}>
+                  <View style={tw`rounded-full border-2 border-red-400 h-10 w-10 flex items-center justify-center mr-2`}>
+                    <Text style={tw`font-bold text-lg text-red-400`}>A+</Text>
+                  </View>
+                  <View style={tw`rounded-full border-2 border-red-400 h-10 w-10 flex items-center justify-center mr-2`}>
+                    <Text style={tw`font-bold text-lg text-red-400`}>O-</Text>
+                  </View>
+                  <View style={tw`rounded-full border-2 border-red-400 h-10 w-10 flex items-center justify-center`}>
+                    <Text style={tw`font-bold text-lg text-red-400`}>AB+</Text>
+                  </View>
+                </View>
+                
+
+                {/* PICTURE */}
+                <View style={tw`flex flex-row justify-around mt-3 mb-2 overflow-hidden	rounded-md`}>
+                  <Image source={require("../../assets/blood-donation.jpg")} style={tw`h-60 w-90 rounded-md`}></Image>
+                </View>
+
+                {/* DIVIDER */}
+                <Divider style={tw`h-0.5 rounded-full mb-2`}/>
+                
+                {/* ICONS */}
+                <View style={tw`w-full flex flex-row`}>
+                  <View style={tw`w-4/5 flex flex-row`}>
+                    <TouchableOpacity>
+                      <Image source={require("../../assets/post-icons/love.png")} style={tw`h-6 w-6 mr-2`}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <Image source={require("../../assets/post-icons/chat-balloon.png")} style={tw`h-6 w-6`}></Image>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={tw`w-1/5`}>
+                    <TouchableOpacity>
+                      <Image source={require("../../assets/post-icons/send.png")} style={tw`w-6 h-6 self-end`}></Image>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
+        
+          </View>
+
+        </View>
+        
+      </ScrollView>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  map: {
-    width: SIZES.width,
-    height: SIZES.height,
-  },
-});
+export default Home
