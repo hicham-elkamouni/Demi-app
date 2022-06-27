bfbb52d947ebfc1c6ad368b16739df9ebc2eb938
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-06-27 14:08:05 +0100

    fetching posts is done :fire:

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,28 +1,33 @@
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import tw from 'twrnc'
-import { Divider } from 'react-native-paper'
-import { PostCard } from '../components/PostCard.jsx'
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import { Divider } from "react-native-paper";
+import { PostCard } from "../components/PostCard.jsx";
 import { FloatingAction } from "react-native-floating-action";
-import { COLORS } from '../constants/theme'
-import { useNavigation } from '@react-navigation/native';
+import { COLORS } from "../constants/theme";
+import { useNavigation } from "@react-navigation/native";
+import { useGetPostsQuery } from "../app/features/donator/postApi.js";
 
 const Home = () => {
-
   const navigation = useNavigation();
+
+  const { data, loading, error } = useGetPostsQuery();
 
   const actions = [
     {
       text: "Create Post",
       icon: require("./../../assets/png/heart.png"),
       name: "NewPost",
-      position: 1
+      position: 1,
     },
-  ]
+  ];
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
-
     <View
       style={{
         flex: 1,
@@ -30,31 +35,28 @@ const Home = () => {
       }}
     >
 
+      <View style={tw`mt-4 mb-6 w-full`}>
+        <ScrollView>
+          {data && 
+            data.map((post, index) => {
+              return (
+                <View key={index}>
+                  <PostCard key={index} post={post}/>
+                </View>
+              );
+            })}
+        </ScrollView>
+      </View>
 
-  
-    <View style={tw`mt-4 mb-6 w-full`}>
-      <ScrollView>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-      </ScrollView>
-    </View>
-
-    
-
-    <FloatingAction
+      <FloatingAction
         actions={actions}
         color={COLORS.primary}
-        onPressItem={name => {
+        onPressItem={(name) => {
           navigation.navigate("AddPost");
         }}
       />
-
-
-
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
